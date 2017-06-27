f9eca12ffdd93cb6e8e68db4d81f835c80907332
Ziceck <frikyequisde@live.com>
2017-06-26 19:20:12 -0500

    Create helloworld.asm

---
+++ MASM/helloworld.asm
@@ -0,0 +1,39 @@
+;author ziceck
+;date: 26/06/2017
+;Code to print hello world with assembly.
+
+.286
+;stack segment
+spila SEGMENT stack
+	DB 32 DUP ('stack___')
+spila ENDS
+
+; data segment
+sdatos SEGMENT
+	hello DB 'Hello Wolrd ','$'
+sdatos ENDS
+
+; code segment
+scodigo SEGMENT 'CODE'
+ASSUME SS:spila, DS:sdatos, CS:scodigo
+MAIN PROC FAR
+
+;default code, not change.
+;_____________________
+    PUSH DS
+    PUSH 0
+    MOV AX, sdatos
+    MOV DS,AX
+	MOV ES,AX
+;_____________________
+	
+	;print hello world
+	MOV AH , 09H   ;09h function in INT 21h
+	LEA DX , hello
+	INT 21H 
+	;end print hello world
+
+	RET
+MAIN ENDP
+scodigo ENDS
+END MAIN
